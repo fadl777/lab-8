b59d3149a378c18e1daa4ff5688f536c344404c8
Fadl Annan <fadl.annan@epita.fr>
2026-05-04 12:15:14 +0200

    exam: q11 - boids: random steer

--- boids_exam.py
+++ boids_exam.py
@@ -81,8 +81,13 @@ class Boid:
 
     # TODO: Implement Random Steering of the velocity vector to create more natural movement
     def _random_steer(self, spread: float = 0.2) -> None:
-        # # Randomly steer a bit to create more natural movement
-        pass
+    angle = math.atan2(self.vy, self.vx)
+    angle += random.uniform(-spread, spread)
+
+    speed = math.hypot(self.vx, self.vy)
+
+    self.vx = math.cos(angle) * speed
+    self.vy = math.sin(angle) * speed
 
 
     # TODO: Implement the three main boid behaviors: separation, alignment, and cohesion
